5ca67fe4b02a5996925f8c46b24edfd32a25429c
Andrea Ceresoni <andrea.ceresoni@gmail.com>
2022-11-06 18:33:59 +0100

    TF

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_public_subnets" {
+  description = "IDs of the VPC's public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "ec2_pubblic" {
+    description = "subnets"
+    value       = module.ec2_instances_public[*].public_ip    
+  }
+  output "key_pair" { 
+  description = "private_key BH" 
+  value       = module.key_pair.private_key_openssh
+  sensitive = true
+}
+
+output "server_id1" {
+  value = toset(module.ec2_instances_public[*].id) 
+}
+
---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  cloud {
+    organization = "4securitas"
+    workspaces {
+      name = "CA-DnsShield"
+    }
+  }
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
